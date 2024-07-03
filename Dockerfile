e2ddb21e98a0135f7b5e0d7d3a69f566c0ce79cf
IKOMA Yoshiki <y@i50.dev>
2024-07-03 22:53:05 +0900

    sshdを動かす

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,6 @@
 FROM alpine:latest
-RUN apk --no-cache --update add bash openssh-client openssh-server autofs cifs-utils samba-client
+RUN apk --no-cache --update add bash openssh openssl ca-certificates autofs cifs-utils samba-client
+ENV SSHD_CONFIG_FILEPATH /etc/ssh/sshd_config
+ENV SSH_AUTHKEYS_FILEPATH /root/.ssh/authorized_keys
 ADD entrypoint.sh /
 CMD ["bash", "/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,30 @@
 
 set -e
 
+SSHD_CONFIG_FILEPATH="${SSHD_CONFIG_FILEPATH:-/etc/ssh/sshd_config}"
+SSH_SERVERKEYS_FILEPATH="${SSH_SERVERKEYS_FILEPATH_LIST:-/etc/ssh/ssh_host_rsa_key}"
+
+## update the sshd_config file
+echo "HostKey ${SSH_SERVERKEYS_FILEPATH}" >> "${SSHD_CONFIG_FILEPATH}"
+echo "PubkeyAuthentication yes" >> "${SSHD_CONFIG_FILEPATH}"
+echo "PasswordAuthentication no" >> "${SSHD_CONFIG_FILEPATH}"
+echo "PermitRootLogin yes" >> "${SSHD_CONFIG_FILEPATH}"
+echo "StrictModes yes" >> "${SSHD_CONFIG_FILEPATH}"
+
+## prepare the ~/.ssh/authorized_keys
+SSH_PUBKEY_FILEPATH="${SSH_PUBKEY_FILEPATH:-/conf/id_key.pub}"
+SSH_AUTHKEYS_FILEPATH="${SSH_AUTHKEYS_FILEPATH:-/root/.ssh/authorized_keys}"
+SSH_AUTHKEYS_BASEDIR="$(dirname ${SSH_AUTHKEYS_FILEPATH})"
+mkdir -p "${SSH_AUTHKEYS_BASEDIR}"
+chmod 700 "${SSH_AUTHKEYS_BASEDIR}"
+cp "${SSH_PUBKEY_FILEPATH}" "${SSH_AUTHKEYS_FILEPATH}"
+chmod 0600 "${SSH_AUTHKEYS_FILEPATH}"
+
+## change the root password
+echo "root:$(openssl rand -hex 12)" | chpasswd
+
+exec /usr/sbin/sshd -D
+
 ssh -NTC ${SSH_OPTS} -i ${SSH_KEY} \
     -o 'UserKnownHostsFile=/dev/null' -o 'StrictHostKeyChecking=no' \
 	-o 'ServerAliveInterval=300' -o 'ExitOnForwardFailure=yes' ${SSH_DEST}
