d4b7223fc51159a90281f2bcd02aa333f9b5e636
IKOMA Yoshiki <y@i50.dev>
2024-07-05 21:42:04 +0900

    entrypoint.sh 修正

--- entrypoint.sh
+++ entrypoint.sh
@@ -24,8 +24,8 @@ chmod 0600 "${SSH_AUTHKEYS_FILEPATH}"
 ## change the root password
 echo "root:$(openssl rand -hex 12)" | chpasswd
 
-service sshd start
-service autofs start
+systemctl start ssh
+systemctl start autofs
 
 ssh -NTC ${SSH_OPTS} -i ${SSH_KEY} \
     -o 'UserKnownHostsFile=/dev/null' -o 'StrictHostKeyChecking=no' \
